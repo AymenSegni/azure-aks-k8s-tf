08a3e8bbd75a2f58c87d3e92700e31332471605a
asegni <segniaymen1@gmail.com>
2019-11-26 00:46:08 +0100

    Provision AKS Kubernetes Cluster

--- src/modules/aks-cluster/main.tf
+++ src/modules/aks-cluster/main.tf
@@ -1,1 +1,115 @@
 
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "${var.environment}-${var.name_suffix}"
+  kubernetes_version  = var.kubernetes_version
+
+  agent_pool_profile {
+    name            = var.agent_pool_name
+    count           = var.node_count
+    vm_size         = var.vm_size
+    os_type         = var.os_type
+    os_disk_size_gb = var.os_disk_size_gb
+    vnet_subnet_id  = var.vnet_subnet_id
+    max_pods        = var.max_pods
+    type            = var.agent_pool_type
+  }
+
+  network_profile {
+    network_plugin     = var.network_plugin
+    network_policy     = "calico"
+    service_cidr       = var.service_cidr
+    dns_service_ip     = "10.0.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  role_based_access_control {
+    enabled = true
+
+    azure_active_directory {
+      client_app_id     = var.aad_client_app_id
+      server_app_id     = var.aad_server_app_id
+      server_app_secret = var.aad_server_app_secret
+      tenant_id         = var.aad_tenant_id
+    }
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      "aadSSH" = "True"
+    },
+  )
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "aks_cluster" {
+  name                       = "${azurerm_kubernetes_cluster.cluster.name}-audit"
+  target_resource_id         = azurerm_kubernetes_cluster.cluster.id
+  log_analytics_workspace_id = var.diagnostics_workspace_id
+
+  log {
+    category = "kube-apiserver"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  log {
+    category = "kube-controller-manager"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  log {
+    category = "cluster-autoscaler"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  log {
+    category = "kube-scheduler"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  log {
+    category = "kube-audit"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
+
