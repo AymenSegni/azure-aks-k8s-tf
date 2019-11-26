1625cb09ee4770a45438839a51159b019ee6b316
asegni <segniaymen1@gmail.com>
2019-11-26 22:54:45 +0100

    AKS deployment

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -18,7 +18,7 @@ module "aks_network" {
 # AKS IDs
   
 module "aks_identities" {
-  source                 = "../modules/aks-identities"
+  source                 = "../modules/aks_identities"
   cluster_name           = var.cluster_name
 }
 
