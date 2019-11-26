2a204a78ad2e357e34810e5142f6ca25eb1de7e1
asegni <segniaymen1@gmail.com>
2019-11-26 20:49:43 +0100

    Logs Analytics main tf

--- src/modules/log_analytics/main.tf
+++ src/modules/log_analytics/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_log_analytics_workspace" "test" {
     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
     name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
-    location            = var.location
+    location            = var.log_analytics_workspace_location
     resource_group_name = var.resource_group_name
     sku                 = var.log_analytics_workspace_sku
 }
