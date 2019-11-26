1de91acddc62f166dbbdcb77a527c14957397eba
asegni <segniaymen1@gmail.com>
2019-11-26 22:30:24 +0100

    aks cluster variabels

--- src/modules/aks-cluster/variables.tf
+++ src/modules/aks-cluster/variables.tf
@@ -4,22 +4,18 @@ variable "dns_suffix" {
 
 variable "location" {
   description = "azure location to deploy resources"
-  default     = "westeurope"
 }
 
 variable "cluster_name" {
   description = "AKS cluster name"
-  default     = "mgmt"
 }
 
 variable "resource_group_name" {
   description = "name of the resource group to deploy AKS cluster in"
-  default     = "run it on cloud"
 }
 
 variable "kubernetes_version" {
   description = "version of the kubernetes cluster"
-  default     = "1.14.6"
 }
 
 variable "api_server_authorized_ip_ranges" {
@@ -40,22 +36,18 @@ variable "agent_pool_type" {
 
 variable "node_count" {
   description = "number of nodes to deploy"
-  default     = 3
 }
 
 variable "vm_size" {
   description = "size/type of VM to use for nodes"
-  default     = "Standard_D15_v2"
 }
 
 variable "os_type" {
   description = "type of OS to run on nodes"
-  default     = "Linux"
 }
 
 variable "os_disk_size_gb" {
   description = "size of the OS disk to attach to the nodes"
-  default     = 512
 }
 
 variable "vnet_subnet_id" {
@@ -64,7 +56,6 @@ variable "vnet_subnet_id" {
 
 variable "max_pods" {
   description = "maximum number of pods that can run on a single node"
-  default     = "100"
 }
 
 #Network Profile config
@@ -87,26 +78,6 @@ variable "client_id" {
 variable "client_secret" {
   description = "azure service principal key for node identity"
   default     = "00000000000000000000000000000000"
-}
-
-# AAD RBAC SP config
-variable "aad_client_app_id" {
-  description = "id of client SP used for AAD binding"
-  default     = "00000000-0000-0000-0000-000000000001"
-}
-
-variable "aad_server_app_id" {
-  description = "id of server SP used for AAD binding"
-  default     = "00000000-0000-0000-0000-000000000002"
-}
-
-variable "aad_server_app_secret" {
-  description = "azure service principal key for AAD RBAC binding"
-  default     = "00000000000000000000000000000000"
-}
-
-variable "aad_tenant_id" {
-  description = "tennant ID for the AAD RBAC binding"
 }
 
 variable "diagnostics_workspace_id" {
