06b40d29a335f76d085e46fdebccefeee8c1e1b0
asegni <segniaymen1@gmail.com>
2019-11-26 20:48:52 +0100

    log analytics vars

---
+++ src/modules/log_analytics/variables.tf
@@ -0,0 +1,17 @@
+variable resource_group_name {
+    default = "Run IT on Cloud"
+}
+
+variable log_analytics_workspace_name {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "eastus"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
