562cf90015bc5fe36e8f94711a0488800cd59be7
asegni <segniaymen1@gmail.com>
2019-11-26 20:43:47 +0100

    log_analytic modules

---
+++ src/modules/log_analytics/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_log_analytics_workspace" "test" {
+    # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
+    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    sku                 = var.log_analytics_workspace_sku
+}
+
+resource "azurerm_log_analytics_solution" "test" {
+    solution_name         = "ContainerInsights"
+    location              = azurerm_log_analytics_workspace.test.location
+    resource_group_name   = var.resource_group_name
+    workspace_resource_id = azurerm_log_analytics_workspace.test.id
+    workspace_name        = azurerm_log_analytics_workspace.test.name
+
+    plan {
+        publisher = "Microsoft"
+        product   = "OMSGallery/ContainerInsights"
+    }
+}
