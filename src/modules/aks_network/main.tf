b881f232fc7783a540b99fd81f9e934b5f5440cc
asegni <segniaymen1@gmail.com>
2019-11-26 01:32:51 +0100

    AKS Network Environment

---
+++ src/modules/aks_network/main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = var.vnet_name
+  address_space       = [var.address_space]
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = var.location
+}
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefix       = var.subnet_cidr
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage", "Microsoft.KeyVault"]
+}
+
+
