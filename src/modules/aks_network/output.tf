77c956778d643bb8da7b6115c26aa71b27f12c39
asegni <segniaymen1@gmail.com>
2019-12-22 20:25:21 +0100

    Tf validate fix network module outputs

--- src/modules/aks_network/output.tf
+++ src/modules/aks_network/output.tf
@@ -1,7 +1,7 @@
 output "aks_subnet_id" {
-  value = azurerm_subnet.aks_vnet.id
+  value = azurerm_subnet.aks_subnet.id
 }
 output "aks_vnet_id" {
-  value = azurerm_subnet.aks_subnet.id
+  value = azurerm_virtual_network.aks_vnet.id
 }
 
