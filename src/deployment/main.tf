73ae5249855f097511e11021581b1386ffe20815
asegni <segniaymen1@gmail.com>
2019-12-22 20:22:12 +0100

    TF Validate fix main deployment

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -1,29 +1,31 @@
 # Cluster Resource Group
 
 resource "azurerm_resource_group" "aks" {
-    name     = var.resource_group_name
-    location = var.location
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # AKS Cluster Network
 
 module "aks_network" {
-  source               = "../modules/aks_network"
-  subnet_name          = var.subnet_name
-  vnet_name            = var.vnet_name
-  resource_group_name  = azurerm_resource_group.aks.name
-  subnet_cidr          = var.subnet_cidr
+  source              = "../modules/aks_network"
+  subnet_name         = var.subnet_name
+  vnet_name           = var.vnet_name
+  resource_group_name = azurerm_resource_group.aks.name
+  subnet_cidr         = var.subnet_cidr
+  location            = var.location
+  address_space       = var.address_space
 }
 
 # AKS IDs
-  
+
 module "aks_identities" {
-  source                 = "../modules/aks_identities"
-  cluster_name           = var.cluster_name
+  source       = "../modules/aks_identities"
+  cluster_name = var.cluster_name
 }
 
 # AKS Log Analytics
-  
+
 module "log_analytics" {
   source                           = "../modules/log_analytics"
   resource_group_name              = azurerm_resource_group.aks.name
@@ -34,21 +36,28 @@ module "log_analytics" {
 
 
 # AKS Cluster
-  
+
 module "aks_cluster" {
- source                   = "../modules/aks-cluster"
- cluster_name             = var.cluster_name
- resource_group_name      = azurerm_resource_group.aks.name
- kubernetes_version       = var.kubernetes_version
- node_count               = var.node_count
- os_disk_size_gb          = "1028"
- max_pods                 = "110"
- vm_size                  = var.vm_size
- vnet_subnet_id           = module.aks_network.aks_subnet_id
- client_id                = module.aks_identities.cluster_client_id
- client_secret            = module.aks_identities.cluster_sp_secret
- diagnostics_workspace_id = module.log_analytics.azurerm_log_analytics_workspace
+  source                   = "../modules/aks-cluster"
+  cluster_name             = var.cluster_name
+  location                 = var.location
+  os_type                  = var.os_type
+  dns_prefix               = var.dns_prefix
+  resource_group_name      = azurerm_resource_group.aks.name
+  kubernetes_version       = var.kubernetes_version
+  node_count               = var.node_count
+  os_disk_size_gb          = "1028"
+  max_pods                 = "110"
+  vm_size                  = var.vm_size
+  vnet_subnet_id           = module.aks_network.aks_subnet_id
+  client_id                = module.aks_identities.cluster_client_id
+  client_secret            = module.aks_identities.cluster_sp_secret
+  diagnostics_workspace_id = module.log_analytics.azurerm_log_analytics_workspace
 }
+
+
+
+
 
 
 
