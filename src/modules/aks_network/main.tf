ec0cb3988688f55b06946bba905c10d80a519059
asegni <segniaymen1@gmail.com>
2019-12-22 20:24:36 +0100

    TF validate fix main network  module

--- src/modules/aks_network/main.tf
+++ src/modules/aks_network/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = var.vnet_name
   address_space       = [var.address_space]
-  resource_group_name = azurerm_resource_group.resource_group.name
+  resource_group_name = var.resource_group_name
   location            = var.location
 }
 resource "azurerm_subnet" "aks_subnet" {
@@ -11,5 +11,7 @@ resource "azurerm_subnet" "aks_subnet" {
   address_prefix       = var.subnet_cidr
   service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage", "Microsoft.KeyVault"]
 }
+
+
 
 
